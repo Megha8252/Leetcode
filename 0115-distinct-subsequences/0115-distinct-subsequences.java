b2137369d784254e97c0e5fde9a80207d46a96a4
Megha <meghakumarisriwastva@gmail.com>
2026-09-06 21:05:05 +0530

    Time: 14 ms (92.72%), Space: 43.2 MB (87.36%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+
+        // dp[j] = number of ways to form t[0..j-1]
+        long[] dp = new long[n + 1];
+        dp[0] = 1; // empty t
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = n; j >= 1; j--) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return (int) dp[n];
+    }
+}
